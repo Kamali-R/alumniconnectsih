2b33c48846c1a3dc93872715ffffd0bfc22a6296
JEYADHARANIK <dharanijeya8@gmail.com>
2025-07-11 11:11:06 +0530

    Added jsonwebtokens for securing routes in login

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import { sendOtp, verifyOtp } from '../controllers/authController.js';
-import { loginUser } from '../controllers/authController.js';
+import { login } from '../controllers/authController.js';
 const router = express.Router();
 
 router.post('/send-otp', sendOtp);       // Step 1
 router.post('/verify-otp', verifyOtp);   // Step 2
-router.post('/login', loginUser);
+router.post('/login', login);
 export default router;
 
 
