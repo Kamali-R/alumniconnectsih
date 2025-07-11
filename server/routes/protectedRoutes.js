4f7b43873775146a8b75386a7c4618fbc59f835c
JEYADHARANIK <dharanijeya8@gmail.com>
2025-07-11 11:32:20 +0530

    Added protected routes

---
+++ server/routes/protectedRoutes.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import { authenticateToken } from '../middleware/authMiddleware.js';
+
+const router = express.Router();
+
+router.get('/dashboard', authenticateToken, (req, res) => {
+  res.json({
+    message: `Welcome ${req.user.role}!`,
+    user: req.user
+  });
+});
+
+export default router;
