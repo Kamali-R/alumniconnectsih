afc0a9bd6c736e7bce36d52ea9a783998e5af097
JEYADHARANIK <dharanijeya8@gmail.com>
2025-07-14 15:28:11 +0530

    Changed

--- server/config/googleAuth.js
+++ server/config/googleAuth.js
@@ -3,14 +3,14 @@ import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import dotenv from "dotenv";
 dotenv.config();
 
-import User from "../models/User.js"; // ✅ Make sure this is the correct path
+import User from "../models/User.js"; 
 
 passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:5000/api/google/callback", // ✅ Must match Google Console
+      callbackURL: "http://localhost:5000/api/google/callback", 
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
